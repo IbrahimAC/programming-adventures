b1e025175eb546f3608ea2f05d928b0d029ddf73
Ibrahim <80414927+IbrahimAC@users.noreply.github.com>
2021-08-02 04:31:31 +0100

    Delete further_practice directory

--- further_practice/multiply.py
+++
@@ -1,9 +0,0 @@
-def multiply(a, b):
-    
-    """Returns the multiplication of the given integers
-       input: int, int
-       output: int"""
-    return a * b
-
-print(multiply(5, 10))
-print(multiply(7, 7))
