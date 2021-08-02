e42e36ac7ded3ca173e73928010aba381650c82e
Ibrahim <80414927+IbrahimAC@users.noreply.github.com>
2021-08-02 18:38:37 +0100

    Add files via upload

---
+++ practice_exams/second_exam.py
@@ -0,0 +1,78 @@
+#This was a second exam taken, only the answers were noted not the questions.
+#Therefore this file is mainly for archiving purposes.
+
+
+#1
+NoNo!!!
+No!!!No!!!'
+
+#2
+error
+
+#3
+23 #(in string format)
+
+#4
+Lo
+Hi
+Hi
+
+#5
+No
+Yes
+
+#6
+10
+8
+
+#7
+8
+
+#8
+(0,1,2)
+
+#9
+0
+1
+2
+3
+
+#10
+wordList = ['Jose', 'Sue', 'Ivan']
+for i in wordList:
+    print(i) and print(i)
+
+#11
+o(1), o(n), o(n²), o(logn)
+
+
+"""Word Document Questions Now"""
+#It won't make much sense as I was simply choosing answers most of the time, using a word document.
+
+#w8
+a - true
+
+#w9
+d - slice
+
+#w10
+c - generics
+
+#w12
+H
+
+#w13
+llo
+
+#w14
+llo world!
+
+#w18
+[786 ,2.23]
+
+#w21
+type error
+int operator and list error
+
+#w23
+d
